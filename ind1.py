3495667dd92f242b32164bb712308f07c2b3c5ce
Nika1411 <niksel99.99@mail.ru>
2020-11-06 01:02:11 +0300

    Индивидуальное задание 1

---
+++ ind1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Вариант17
+# Ввести список А из 10 элементов, найти квадраты элементов кратных 4 и их количество.Преобразованный массив вывести
+import sys
+
+if __name__ == '__main__':
+    A = tuple(map(int, input().split()))
+    if len(A) != 10:
+        print("Неверный размер списка", file=sys.stderr)
+        exit(1)
+    L = []
+    for i in A:
+        if i % 4 == 0:
+            i *= i
+        L.append(i)
+    print(tuple(L))
